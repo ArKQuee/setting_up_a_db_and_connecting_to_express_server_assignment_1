c46c95a3551c66326b4f690e13ddbe77f8b2a499
Sajin Saji George <sajinsajigeorge@gmail.com>
2025-02-18 10:48:52 +0530

    Setup backend with MongoDB connection

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const express = require('express');
-const { resolve } = require('path');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-const port = 3010;
+app.use(express.json()); 
 
-app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Connected to database");
+  } catch (error) {
+    console.error(`❌ Error connecting to database: ${error.message}`);
+    process.exit(1); 
+  }
+};
+
+
+connectDB();
+
+
+app.get("/", (req, res) => {
+  res.send("Customer Management System Backend is Running...");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
